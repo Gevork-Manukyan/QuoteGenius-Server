581e491086958058880cca316f5692354f6b5fe4
Gevork Manukyan <Manukyan.Gevork01@gmail.com>
2023-05-16 11:25:49 -0700

    put new quote with author

--- QuoteGenius-Server/Controllers/QuoteController.cs
+++ QuoteGenius-Server/Controllers/QuoteController.cs
@@ -59,6 +59,36 @@ public class QuoteController : ControllerBase
         return quotesWithAuthors;
     }
 
+    // GET: api/Quotes/WithAuthor/{id}
+    [HttpGet("WithAuthor/{id}")]
+    [Authorize]
+    public async Task<ActionResult<QuoteWithAuthor>> GetQuoteWithAuthor(int id)
+    {
+        var quoteWithAuthor = await _context.Quotes
+            .Include(q => q.Author)
+            .Where(q => q.Id == id)
+            .Select(q => new QuoteWithAuthor
+            {
+                QuoteId = q.Id,
+                QuoteText = q.Text,
+                DatePublished = q.DatePublished,
+                AuthorId = q.Author.Id,
+                AuthorName = q.Author.Name,
+                AuthorBirthday = q.Author.Birthday,
+                AuthorRace = q.Author.Race,
+                AuthorGender = q.Author.Gender
+            })
+            .SingleOrDefaultAsync();
+
+        if (quoteWithAuthor == null)
+        {
+            return NotFound();
+        }
+
+        return quoteWithAuthor;
+    }
+
+
     // PUT: api/Quotes/5
     [HttpPut("{id}")]
     [Authorize]
@@ -87,6 +117,58 @@ public class QuoteController : ControllerBase
 
         return NoContent();
     }
+
+    // PUT: api/Quotes/WithAuthor/{id}
+    [HttpPut("WithAuthor/{id}")]
+    [Authorize]
+    public async Task<IActionResult> PutQuoteWithAuthor(int id, QuoteWithAuthor quoteWithAuthor)
+    {
+        if (id != quoteWithAuthor.QuoteId)
+        {
+            return BadRequest();
+        }
+
+        var quote = await _context.Quotes.FindAsync(id);
+
+        if (quote == null)
+        {
+            return NotFound();
+        }
+
+        var author = await _context.Authors.FindAsync(quoteWithAuthor.AuthorId);
+
+        if (author == null)
+        {
+            return NotFound();
+        }
+
+        // Update the quote properties
+        quote.Text = quoteWithAuthor.QuoteText;
+        quote.DatePublished = quoteWithAuthor.DatePublished;
+
+        // Update the author properties
+        author.Name = quoteWithAuthor.AuthorName;
+        author.Birthday = quoteWithAuthor.AuthorBirthday;
+        author.Race = quoteWithAuthor.AuthorRace;
+        author.Gender = quoteWithAuthor.AuthorGender;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!QuoteExists(id))
+            {
+                return NotFound();
+            }
+
+            throw;
+        }
+
+        return NoContent();
+    }
+
 
     // POST: api/Quotes
     [HttpPost]
